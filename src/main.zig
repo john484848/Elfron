26991d5da7a34f0b686c3cb9d673dcbd52b79195
Jonathan Kloepfer <kloepj@rpi.edu>
2022-11-05 17:02:55 +0000

    More work and swtching to hashing for the columns

--- src/main.zig
+++ src/main.zig
@@ -3,27 +3,48 @@ const testing = std.testing;
 var gpa= std.heap.GeneralPurposeAllocator(.{}){};
 const allocator= gpa.allocator();
 pub const dtype= enum {int, float, bool, string};
-const cell= union {
+const cell= union(dtype) {
     int: i64,
     float:f64,
     bool: bool,
-    string: [*]u8,
+    string: []u8,
 };
-const row= struct{
-    Row:[*]cell,
-    title:[*]u8,
-    Size:u64,
+const column= struct{
+    Row:[]cell,
+    title:[]u8,
+    size:u64,
 };
-pub const dataframe= struct{
-    Rows:[*]row,
-    Size:u64,
-    ///pub fn addColumn() void{
-    ///}
+pub const DataFrame= struct{
+    Rows:std.StringHashMap(column),
+    size:u64,
+    pub fn addColumn(self: *DataFrame,ctype:dtype, title: []const u8) void{
+        self.size+=1;
+        var basic_union:cell=undefined;
+        switch(ctype){
+            .int => {basic_union=cell{.int=7};},
+            .float =>{basic_union=cell{.float=7};}, 
+            else => {unreachable;},
+        }
+
+        std.debug.print("Type:{}\n",.{ctype});
+        std.debug.print("Type Union: {}\n",.{basic_union});
+        std.debug.print("Type Union: {s}\n",.{title});
+
+    }
 };
-pub fn newdf() dataframe{
-    return dataframe{.Rows=undefined, .Size=0};
+pub fn newdf() DataFrame{
+    return DataFrame{.Rows=std.StringHashMap(column).init(allocator), .size=0};
+}
+pub fn main() void{ 
+   var df1= newdf();
+   var s="HI";
+   df1.addColumn(dtype.int,s);
+   df1.addColumn(dtype.float,s);
 }
 test "basic add functionality" {
     var df1= newdf();
-    try testing.expect(df1.Size==0);
+    var s="HI";
+    try testing.expect(df1.size==0);
+    df1.addColumn(dtype.int,s);
+    try testing.expect(df1.size==1);
 }
