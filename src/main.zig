2930a06655c93fe92564a7a94bdc115ef82666d6
Jonathan Kloepfer <kloepj@rpi.edu>
2022-11-05 03:11:04 +0000

    Struct Definition

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,29 @@
 const std = @import("std");
 const testing = std.testing;
-
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+var gpa= std.heap.GeneralPurposeAllocator(.{}){};
+const allocator= gpa.allocator();
+pub const dtype= enum {int, float, bool, string};
+const cell= union {
+    int: i64,
+    float:f64,
+    bool: bool,
+    string: [*]u8,
+};
+const row= struct{
+    Row:[*]cell,
+    title:[*]u8,
+    Size:u64,
+};
+pub const dataframe= struct{
+    Rows:[*]row,
+    Size:u64,
+    ///pub fn addColumn() void{
+    ///}
+};
+pub fn newdf() dataframe{
+    return dataframe{.Rows=undefined, .Size=0};
 }
-
 test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+    var df1= newdf();
+    try testing.expect(df1.Size==0);
 }
